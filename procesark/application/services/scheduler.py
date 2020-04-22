be3617609317c3217fa385599e58907a52e05c73
Esteban Echeverry <eecheverry@nubark.com>
2020-04-22 09:57:28 -0500

    Define scheduler subscribe, start and stop

--- procesark/application/services/scheduler.py
+++ procesark/application/services/scheduler.py
@@ -12,16 +12,32 @@ class Scheduler(ABC):
         "Schedule method to be implemented."
 
     @abstractmethod
-    async def subscribe(self, Callback: Callback) -> None:
+    async def subscribe(self, callback: Callback) -> None:
         "Subscribe method to be implemented."
+
+    @abstractmethod
+    async def start(self) -> None:
+        "Start method to be implemented."
+
+    @abstractmethod
+    async def stop(self) -> None:
+        "Start method to be implemented."
 
 
 class StandardScheduler(Scheduler):
     def __init__(self) -> None:
-        self.triggers = []
+        self.triggers = set()
+        self.subscribers = set()
+        self.active = False
 
     async def schedule(self, triggers: List[Trigger]) -> None:
-        self.triggers = triggers
+        self.triggers.update(triggers)
 
-    async def subscribe(self, Callback: Callback) -> None:
-        "Subscribe method to be implemented."
+    async def subscribe(self, callback: Callback) -> None:
+        self.subscribers.add(callback)
+
+    async def start(self) -> None:
+        pass
+
+    async def stop(self) -> None:
+        pass
--- tests/application/services/test_scheduler.py
+++ tests/application/services/test_scheduler.py
@@ -6,21 +6,36 @@ def test_scheduler_methods() -> None:
     methods = Scheduler.__abstractmethods__  # type: ignore
     assert 'schedule' in methods
     assert 'subscribe' in methods
+    assert 'start' in methods
+    assert 'stop' in methods
 
 
 def test_standard_scheduler_implementation() -> None:
     assert issubclass(StandardScheduler, Scheduler)
 
 
-def test_standard_scheduler_implementation() -> None:
-    assert issubclass(StandardScheduler, Scheduler)
+def test_standard_scheduler_default_attributes() -> None:
+    scheduler = StandardScheduler()
+    assert scheduler.triggers == set()
+    assert scheduler.subscribers == set()
+    assert scheduler.active is False
 
 
 async def test_standard_scheduler_schedule() -> None:
     triggers = [Trigger(id='001', process_id='001'),
                 Trigger(id='002', process_id='002')]
-    scheduler = StandardScheduler()
 
+    scheduler = StandardScheduler()
     await scheduler.schedule(triggers)
 
-    assert scheduler.triggers == triggers
+    assert scheduler.triggers == set(triggers)
+
+
+async def test_standard_scheduler_subscribe() -> None:
+    async def mock_callable(trigger: Trigger) -> None:
+        pass
+
+    scheduler = StandardScheduler()
+    await scheduler.subscribe(mock_callable)
+
+    assert mock_callable in scheduler.subscribers
