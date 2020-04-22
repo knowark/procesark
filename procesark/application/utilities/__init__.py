910becaf120434989c86c4652901a6169c2dba1a
Esteban Echeverry <eecheverry@nubark.com>
2020-04-21 23:40:43 -0500

    Define cronable utility

--- procesark/application/utilities/__init__.py
+++ procesark/application/utilities/__init__.py
@@ -1,5 +1,6 @@
 from .exceptions import *
 from .types import *
+from .cronable import cronable
 from .query_parser import QueryParser
 from .tenancy import Tenant, TenantProvider, StandardTenantProvider
 from .auth import User, AuthProvider, StandardAuthProvider
