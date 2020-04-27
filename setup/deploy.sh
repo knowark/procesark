d43242a99cac726a3f9bf5566249badd3d91430a
Esteban Echeverry <eecheverry@nubark.com>
2020-04-26 21:03:57 -0500

    Redirect output to file and stdout

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -20,5 +20,5 @@ lxc exec $CONTAINER -- apt autoremove -y
 
 echo "Deploy with Ansible Pull..."
 
-lxc exec $CONTAINER -- ansible-pull --connection=local -i 127.0.0.1, \
-    -U $REPOSITORY -d /var/git/$CONTAINER $PLAYBOOK 2>&1 | tee deploy.log
+lxc exec $CONTAINER -- bash -c "ansible-pull --connection=local -i 127.0.0.1, \
+    -U $REPOSITORY -d /var/git/$CONTAINER $PLAYBOOK 2>&1 | tee deploy.log"
